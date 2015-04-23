bb49c234e5cab41ecb48428f6ac20112d237d1fd
Erik Schierboom <erik_schierboom@hotmail.com>
2015-04-23 09:26:19 +0200

    Allow header to include dynamic version

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
-var gulp = require('gulp'),
-    yaml = require('js-yaml'),
-    fs = require('fs'),
-    mochaPhantomJS = require('gulp-mocha-phantomjs'),
-    config = yaml.safeLoad(fs.readFileSync('./config.yaml', 'utf8')),
-    plugins = require('gulp-load-plugins')();
+var gulp    = require('gulp'),
+    yaml    = require('js-yaml'),
+    fs      = require('fs'),
+    config  = yaml.safeLoad(fs.readFileSync('./config.yaml', 'utf8')),
+    plugins = require('gulp-load-plugins')(),
+    replace = require('gulp-replace'),
+    mochaPhantomJS = require('gulp-mocha-phantomjs');
 
 function updateVersion(importance) {
   return gulp.src(['./package.json', './bower.json'])
-    .pipe(plugins.bump({type: importance}))
+    .pipe(plugins.bump({ type: importance }))
     .pipe(gulp.dest('./'));
 }
 
-gulp.task('patch', function() { return updateVersion('patch'); });
-gulp.task('feature', function() { return updateVersion('minor'); });;
-gulp.task('release', function() { return updateVersion('major'); })
+gulp.task('patch',   function() { return updateVersion('patch'); });
+gulp.task('feature', function() { return updateVersion('minor'); });
+gulp.task('release', function() { return updateVersion('major'); });
 
 gulp.task('webserver', function () {
   return gulp.src('.')
@@ -21,16 +22,18 @@ gulp.task('webserver', function () {
 });
 
 gulp.task('test', function () {
-    return gulp
+  return gulp
     .src('test/runner.html')
     .pipe(mochaPhantomJS());
 });
 
 gulp.task('js', function () {
+  var pkg = require('./package.json');
+
   gulp.src("./index.js")
-    .pipe(plugins.header(config.header,
-        { pkg: require('./package.json'), now: new Date() }))
+    .pipe(plugins.header(config.header, { pkg: pkg, now: new Date() }))
     .pipe(plugins.footer(config.footer))
+    .pipe(replace('{{ version }}', pkg.version))
     .pipe(plugins.rename('knockout-pre-rendered.js'))
     .pipe(gulp.dest('./dist'))
     .pipe(plugins.uglify())
